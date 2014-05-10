e73decdde4de054153de149249c62f543fe12672
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-05-10 12:31:18 -0400

    breaking sents

--- transform/transformer.py
+++ transform/transformer.py
@@ -2,12 +2,16 @@ import os
 import re
 
 root = os.path.abspath('./')
-
 orig = os.path.join(root,'original/')
-
 files = os.listdir(orig)
-
 for file in files:
     with open(orig + file) as f:
         r = f.read()
-        print(r)
+        #readlines
+        #lines = f.readlines()
+        #lines = lines[:-3]
+        #print(str(lines[1:22]))
+
+        n = re.sub(r'([a-z]+)(\.|;|\?)\s([\w]+)', r'\1\2\n\3', r)
+        d = re.sub(r'\[.+\]', r'', n)
+        print(d)
