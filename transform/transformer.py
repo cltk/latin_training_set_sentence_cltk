94fa90ec4c1d33c895898470dc65729a3c2f1849
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-05-11 00:22:36 -0400

    verrem stripped

--- transform/transformer.py
+++ transform/transformer.py
@@ -4,6 +4,7 @@ import re
 root = os.path.abspath('./')
 orig = os.path.join(root,'training_set/')
 files = os.listdir(orig)
+training_set = ''
 word_count = 0
 for file in files:
     with open(orig + file) as f:
@@ -16,7 +17,8 @@ for file in files:
         no_file_data_2 = re.sub(r'Cicero|\: In Catilinam .*|ORATIO.*', r'', no_file_data_1)
         no_file_data_3 = re.sub(r'Augstine|\: Confessions .*|AUGUSTINI.*', r'', no_file_data_2)
         no_file_data_4 = re.sub(r'Cato|\: M. PORCI CATONIS .*|DE AGRI CVLTVRA', r'', no_file_data_3)
-        no_double_end_ns = re.sub(r'\n\s*', '\n', no_file_data_2)
+        no_file_data_5 = re.sub(r'In Verrem .*|ACTIONIS IN C. VERREM .*|LIBER .*', r'', no_file_data_4)
+        no_double_end_ns = re.sub(r'\n\s*', '\n', no_file_data_5)
         no_double_start_ns = re.sub(r'\s*\n', '\n', no_double_end_ns)
         no_ascii_junk = re.sub(r'@|#|\$|%|\^|&|\*', '', no_double_start_ns)
         print(no_ascii_junk)
